f6c66fb06429c3dbb264c47b8873ff46142513a8
Ben Newman <benjaminjnewman@gmail.com>
2016-05-30 12:26:03 -0500

    Add conditional logic to handle edge cases

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,21 +3,37 @@
 def encrypt(string)
   counter = 0
   while counter < (string.length)
-    string[counter] = string[counter].next
-    counter +=1
+    if string[counter] == " "
+      counter +=1
+    elsif string[counter] == "z"
+      string[counter] = "a"
+      counter +=1
+    else
+      string[counter] = string[counter].next
+      counter +=1
+    end
   end
-  puts string
+  p string
 end
+
+#Decrypt method
 
 def decrypt(string)
   counter = 0
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   while counter < (string.length)
-    string[counter] = alphabet[(alphabet.index(string[counter]))-1]
-    counter +=1
+    if string[counter] == " "
+      counter +=1
+    elsif string[counter] == "a"
+      string[counter] = "z"
+      counter +=1
+    else
+      string[counter] = alphabet[(alphabet.index(string[counter]))-1]
+      counter +=1
+    end
   end
-  puts string
+  p string
 end
 
 
-encrypt("zed")
+decrypt(encrypt("swordfish"))
