e510184ebed2628b3e312bd1805b00e13e64c17e
Ben Newman <benjaminjnewman@gmail.com>
2016-05-31 08:19:56 -0500

    Add new employee questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+#Questions to gather new employee data
+
+puts "What is your name?"
+werename = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birthyear = gets.chomp
+
+puts "Do you like garlic bread?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in health insurance?"
+insurance = gets.chomp
