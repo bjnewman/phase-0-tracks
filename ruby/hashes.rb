58ebfe7358de6d127576824dd2f5af4bdc526d3f
Ben Newman <benjaminjnewman@gmail.com>
2016-06-05 17:55:53 -0500

    Debug other reason logic in hashes.rb at lines 40-44

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,41 +1,52 @@
+p "Test"
 #Interior Design Client Info
 #Set default hash
-client_bio = {name: "", age: 1, decor_theme: "", childnum: = 0, new_baby: false, new_property: false, other_reason: "", budget: = 50000, paid: = 0}
+client_bio = {
+  name: "", 
+  age: 1, 
+  decor_theme: "", 
+  childnum: 0, 
+  new_baby: false, 
+  new_property: false, 
+  other_reason: "", 
+  budget: 50000, 
+  paid: 0
+}
 p client_bio
 #Ask user for client name (convert to string) and add to hash as value for key name
-puts = "Please enter the client's name:"
+puts "Please enter the client's name:"
   client_bio[:name] = gets.chomp
 #Get client age (convert to integer) and add for key age
-puts = "Please enter the client's age:"
+puts "Please enter the client's age:"
   client_bio[:age] = gets.to_i
 #Get decor theme (convert to string) and add for key decor_theme
-puts = "Please enter the client's preferred decor theme:"
+puts "Please enter the client's preferred decor theme:"
   client_bio[:name] = gets.chomp
 #Get current number of kids (convert to int) and add for key childnum
-puts = "Please enter the number of children's rooms on the property:"
+puts "Please enter the number of children's rooms on the property:"
   client_bio[:childnum] = gets.to_i
 #Ask if project includes nursery (convert to boolean) and add for new_baby
-puts = "Does this project include a nursery for a baby:"
+puts "Does this project include a nursery for a baby:"
   nursery = gets.chomp
   if nursery == "Yes"
   client_bio[:new_baby] = true
   end
 #Ask if project includes purchase of new property (convert to bool) and add for key new_property
-puts = "Does this project include the purchase of a new property:"
+puts "Does this project include the purchase of a new property:"
   newprop = gets.chomp
   if newprop == "Yes"
   client_bio[:new_property] = true
   end
-#If project doesnt include nursery or new property ask for brief description (convert to string and add for key other_reason)
-if ~(client_bio[:new_baby] || client_bio[:new_property])
-  puts = "Please include a brief description of the reason for hiring our firm:"
+#Unless project includes nursery or new property ask for brief description (convert to string and add for key other_reason)
+unless client_bio[:new_baby] && client_bio[:new_property]
+  puts "Please include a brief description of the reason for hiring our firm:"
   client_bio[:other_reason] = gets.chomp
 end
 #Get estimated project budget
-puts = "Please enter the estimated project budget:"
+puts "Please enter the estimated project budget:"
   client_bio[:budget] = gets.to_i
 #Get total payment received
-puts = "Please enter total payment received:"
+puts "Please enter total payment received:"
   client_bio[:budget] = gets.to_i
 #Print client_bio to screen
 p client_bio
