ec57d4c1824b5c956092219ee7cbb4334eda0014
Ben Newman <benjaminjnewman@gmail.com>
2016-06-05 21:38:28 -0500

    Add vowel and consonant handling for codename algorithm, some edge cases, completing 5.6.0

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,16 +10,39 @@ end
 p nameswap(spy)
 
 #split name into arrays
-name_array = nameswap(spy).split('')
+name_array = nameswap(spy).chars
+p name_array
+
+vowels = ["a", "e", "i", "o", "u" ]
+near_vowels = ["d", "h", "n", "t"]
+
+next_name_array = name_array.map do |letter|
+#if letter is a space do nothing
+if letter == " "
+  letter = " "
+#if lettter is vowel increment by one
+
+elsif vowels.include?(letter.downcase)
+  letter.tr('aeiouAEIOU', 'eiouaEIOUA')
+  
+#if letter is w/in 1 of vowel should skip [d,D,h,H,n,N,t,T] -> [f,F,j,J,p,P,v,V]
+
+elsif near_vowels.include?(letter.downcase)
+  letter = letter.tr('dDhHnNtT', 'fFjJpPvV')
+
+#[z,Z] -> [b,B]
+elsif letter.downcase == "z"
+  letter.tr('zZ', 'bB')
 
 #by default increment each letter by one
-next_name_array = name_array.map {|letter| letter.next!}
+else 
+  letter.next
+end
+end
 
-p next_name_array 
-
-#if letter is w/in 1 of vowel should skip [d,D,h,H,n,N,t,T] -> [f,F,j,J,p,P,u,U]
-#other edge case keep spaces
-#[z,Z] -> [a,A]
+p next_name_array
+codename = next_name_array.join
+p codename
 
 
 #target is "Vussit Gimodoe"
