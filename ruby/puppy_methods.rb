240b68d3806c1fefb7b923b8bbb33fd9774553c6
Ben Newman <benjaminjnewman@gmail.com>
2016-06-07 18:51:06 -0500

    Add instance methods and driver code to test for 6.2 rel-0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,6 @@
+# Define puppy class and define fetch instance method.
+# Insert driver code to test puppy class and instance method.
+
 class Puppy
 
   def fetch(toy)
@@ -5,5 +8,27 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    "Woof!" * integer
+  end
+
+  def roll_over
+    "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    7 * human_years
+  end
+
+  def catch_item(item)
+    "I caught the #{item}!"
+  end
+
 end
 
+fido = Puppy.new
+p fido.fetch("bone")
+p fido.speak(3)
+p fido.roll_over
+p fido.dog_years(2)
+p fido.catch_item("ball")
