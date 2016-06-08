cf77fbe4c8530a12eaf0396d5958f862223efa1c
Ben Newman <benjaminjnewman@gmail.com>
2016-06-07 19:55:09 -0500

    Create new class, add initialize method, add array to store new members of class and iterate over array with new instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,39 +1,83 @@
-# Define puppy class and define fetch instance method.
+# # Define puppy class and define fetch instance method.
 
 
-class Puppy
+# class Puppy
+
+#   def initialize
+#     puts "Initializing new puppy instance..."
+#   end
+
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+
+#   def speak(integer)
+#     "Woof!" * integer
+#   end
+
+#   def roll_over
+#     "*rolls over*"
+#   end
+
+#   def dog_years(human_years)
+#     7 * human_years
+#   end
+
+#   def catch_item(item)
+#     "I caught the #{item}!"
+#   end
+
+# end
+
+# # Insert driver code to test puppy class and instance methods.
+# fido = Puppy.new
+# p fido.fetch("bone")
+# p fido.speak(3)
+# p fido.roll_over
+# p fido.dog_years(2)
+# p fido.catch_item("ball")
+
+#Release2 design and implement own class with initialize and two instance methods
+
+class DeLorean
 
   def initialize
-    puts "Initializing new puppy instance..."
+    p "Intializing flux capacitor..."
   end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+  def time_travel(traveler)
+    p "#{traveler} has left the present and have entered the mysterious universe of time travel!"
   end
 
-  def speak(integer)
-    "Woof!" * integer
+  def power_charge
+    "You need more that 1.21 gigawatts"
   end
-
-  def roll_over
-    "*rolls over*"
-  end
-
-  def dog_years(human_years)
-    7 * human_years
-  end
-
-  def catch_item(item)
-    "I caught the #{item}!"
-  end
+  
 
 end
 
-# Insert driver code to test puppy class and instance methods.
-fido = Puppy.new
-p fido.fetch("bone")
-p fido.speak(3)
-p fido.roll_over
-p fido.dog_years(2)
-p fido.catch_item("ball")
+# #driver code to test Delorean instance methods
+# marty = DeLorean.new
+# marty.time_travel("Marty")
+# marty.power_charge(1)
+
+
+time_travelers = []
+i = 0
+until i == 50
+  doc_brown = DeLorean.new
+  time_travelers[i] = doc_brown #stores new DeLorean in the time travelers array
+  i += 1
+end
+
+#p time_travelers  #debug 
+
+#iterates over array to call instance methods on each member of DeLorean class
+time_travelers.each {|traveler| 
+p traveler.time_travel(traveler)
+p traveler.power_charge
+
+}
+
+
