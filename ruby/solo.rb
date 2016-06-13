a6c8bb6bad6700943015ea7773543d005811c846
Ben Newman <benjaminjnewman@gmail.com>
2016-06-12 19:17:43 -0500

    Add pseudocode design for hobbit class for release 0

---
+++ ruby/solo.rb
@@ -0,0 +1,16 @@
+#Release 0 design a class with >= 3 attributes of >=2 data types that vary from instance
+#define at least 3 methods 1 of which takes an argument
+
+create class of Hobbits, attributes (age is integer, name is string, hunger is boolean, courage is boolean)
+methods 
+eat 
+print "Mmm, breakfast" 
+eat_again 
+print"Mmm, second breakfast, now I'm full!" change hunger to false
+hero_stuff IF courage is true print "Hobbit fought the real battle in that tunnel alone, before Hobbit ever saw the vast danger that lay in wait.' - JRR Tolkien, The Hobbit"
+           ELSE print "It's a dangerous business, going out your front door. -JRR Tolkien, The Hobbit"
+initialize (name, age)
+default hunger to true and courage to false
+
+
+
