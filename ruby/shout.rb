3e5c5569d8f81bcf189f0991a007ed821cd17b2d
Ben Newman <benjaminjnewman@gmail.com>
2016-06-12 23:28:37 -0500

    Refactor shout module to mixin for Driver and Parrot Classes and tested successfully with driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,41 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :( "
+#   end
+#   def self.yell_happily(more_words)
+#     more_words + "!!!" + " Woohoo!"
+#   end
+# end 
+
+# Shout.yell_angrily("LOUD NOISES")
+# Shout.yell_happily("USA USA USA")
+
+#commented out old code for Release 3, convert to mixin
+#write two classes and include shout in classes
+#add driver code to test module with instances of each class
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :( "
   end
-  def self.yell_happily(more_words)
+  def yell_happily(more_words)
     more_words + "!!!" + " Woohoo!"
   end
 end 
 
-Shout.yell_angrily("LOUD NOISES")
-Shout.yell_happily("USA USA USA")
+class Angry_Driver
+  include Shout
+end
+
+class Parrot
+  include Shout
+end
+
+#driver code to create instances of Angry_Driver and Parrot and test shout methods
+daniel = Angry_Driver.new
+p daniel.yell_angrily("Hey you cut met off")
+p daniel.yell_happily("What a beautiful day for a drive, you jerks")
+
+polly = Parrot.new
+p polly.yell_angrily("I'm tired of all these darn crackers!")
+p polly.yell_happily("YES MORE CRACKERS")
