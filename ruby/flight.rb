3fa875419c4d8e7f39ab2775483c059b3e3dd025
Ben Newman <benjaminjnewman@gmail.com>
2016-06-12 23:19:44 -0500

    Add mixin sample syntax to flight.rb as example for 6.5 release 2

---
+++ ruby/flight.rb
@@ -0,0 +1,19 @@
+#Modules have to go above the class declarations
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending to #{altitude} feet..."
+  end
+end
+
+class Bird
+  include Flight
+end
+
+class Plane
+  include Flight
+end
+
+bird = Bird.new
+bird.take_off(800)
+plane = Plane.new
+plane.take_off(30000)
