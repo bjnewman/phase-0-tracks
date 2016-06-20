77047c60444d63b5cfd81f98d33d9c72ddb3b05e
Ben Newman <benjaminjnewman@gmail.com>
2016-06-20 07:22:19 -0500

    Add arrays of horse names and colors, driver code to add one to each array and print

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+// declare arrays of colors and names for horses, add driver code to add one to each array
+var horseColors = ["brown", "grey", "red", "striped"];
+
+var horseNames = ["Batman", "The Angry One", "Sneezy", "Frank"];
+
+horseColors.push("spotted");
+horseNames.push("Jenny");
+
+console.log(horseColors);
+console.log(horseNames)
