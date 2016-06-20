3667341951d1cdafc8ae0e35407730d4da6f899f
Ben Newman <benjaminjnewman@gmail.com>
2016-06-20 07:39:36 -0500

    Add code to loop through horseNames to create horses object with horseColors as values

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,5 +6,15 @@ var horseNames = ["Batman", "The Angry One", "Sneezy", "Frank"];
 horseColors.push("spotted");
 horseNames.push("Jenny");
 
-console.log(horseColors);
-console.log(horseNames)
+// console.log(horseColors);
+// console.log(horseNames);
+
+//create object from horseNames and horseColors, names as keys
+//Pseudocode - create horses blank object as placeholder, loop through horsenames and add the first name as key with first color as value
+//and loop until the length of the horseNames array which is given as equal to the horseColors, return the object
+var horses = {}
+for (var i = 0; i < horseNames.length; i++) {
+    horses[horseNames[i]] = horseColors[i];
+}
+
+console.log(horses)
