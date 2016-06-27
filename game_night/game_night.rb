fba8363d5cb2230eaf7bf6f52165a43f8c7c1e16
Ben Newman <benjaminjnewman@gmail.com>
2016-06-27 08:47:01 -0500

    Create game night project folder with ruby and sqlite3 db: Add game_night.rb with pseudocode outline for creating game night league table Add driver code to require sqlite3 gem and create 2016game_night.db

---
+++ game_night/game_night.rb
@@ -0,0 +1,32 @@
+#overall goal create standings for board game night
+require 'sqlite3'
+
+#driver code to initialize db
+#db = SQLite3::Database.new("2016game_night.db")
+
+#create UI for user to view standings
+#print data from 2016game_night.db
+#display 
+
+#edit standings
+  #choose which player's stats to edit
+  #choose which stat to update
+  #enter new value
+
+#enter match result
+# => who played
+# => which game
+# => get results (1st,2nd,3rd)
+
+#add new player
+  #name
+  #any points to enter
+  #if yes which game
+  #more points?
+
+#view player stats
+  #display overall standing, standing in each game, win % overall and each game and sortable, 
+
+
+
+
