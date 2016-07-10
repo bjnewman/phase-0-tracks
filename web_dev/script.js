c4259d50de37f915bbf35f7159a4187fcbf771f4
Ben Newman <benjaminjnewman@gmail.com>
2016-07-10 16:29:34 -0500

    Setup add element js function in script.js

--- web_dev/script.js
+++ web_dev/script.js
@@ -7,5 +7,14 @@ function changeParagraphStyle (event) {
   paragraph.style.backgroundColor = "black";
 }
 
+function addElement (event) {
+  console.log("inside addElement function");
+  var p = document.createElement("p");
+  p.textContent = "Nice to meet you.";
+  document.body.appendChild(p);
+}
+
+addElement();
+
 var mybutton = document.getElementById("change-style");
 mybutton.addEventListener("click", changeParagraphStyle);
