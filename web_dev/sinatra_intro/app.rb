615e2d8542f40afc53c72b77bfda986dce26bcc0
Ben Newman <benjaminjnewman@gmail.com>
2016-07-10 17:18:00 -0500

    Add contact route to display address as well as: add great job route to return personalized congrats if available or default if not add route that uses params to add two numbers to complete 9_4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,28 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write /contact route that displays an address 
+
+get '/contact' do 
+  "111 Main Street <br> Evanston, IL 60201"
+end 
+
+# write /great_job route that takes name as query param 
+# if query param not present, the route says 'good job!'
+
+get '/great_job' do 
+  name == params[:name]
+  if name 
+    "Good job, #{name}!"
+  else 
+    "Good job!"
+  end 
+end 
+
+# route that uses route parameters to add 2 nums and respond with the result 
+# when will the data need to be (or arrive as) a string? 
+get '/:number1/plus/:number2' do 
+  "#{params[:number1].to_i + params[:number2].to_i}"
+end
+
